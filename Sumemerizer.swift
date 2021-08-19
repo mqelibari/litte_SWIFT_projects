310163ec455e42317de7cf1015df25177a2109cb
Fatmir Qelibari <lgit-kontakt@fatmir.qelibari.de>
2021-08-19 19:11:33 +0200

    Summerize numbers

---
+++ Sumemerizer.swift
@@ -0,0 +1,17 @@
+//
+//  Sumemerizer.swift
+//  learnSwift
+//
+//  Created by Fatmir Qel on 19.08.21.
+//
+
+import Foundation
+
+
+func summerizer(_ num: Int) -> Int{
+    var result = 0
+    for di in 0...num{
+        result += di
+    }
+    return result
+}
