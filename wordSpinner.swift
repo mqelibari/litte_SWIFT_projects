5a35451dd91bf707e988771d587145b7c8d04217
Fatmir Qelibari <lgit-kontakt@fatmir.qelibari.de>
2021-08-20 10:21:49 +0200

    Spin all words longer then 5

---
+++ wordSpinner.swift
@@ -0,0 +1,21 @@
+//
+//  wordSpinner.swift
+//  learnSwift
+//
+//  Created by Fatmir Qel on 20.08.21.
+//
+
+import Foundation
+
+func spinWords(in sentence: String) -> String {
+    var my_Array: [String] = []
+    my_Array = sentence.components(separatedBy: " ")
+    for (idx, word) in my_Array.enumerated(){
+        if word.count < 5{
+            continue
+        }else{
+            my_Array[idx] = String(my_Array[idx].reversed())
+        }
+    }
+    return my_Array.joined(separator: " ")
+}
